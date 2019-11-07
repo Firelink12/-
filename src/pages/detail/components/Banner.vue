aa5a1ab6a92e6f5d4634252dc635441fc5734768
Firelink12 <396645172@qq.com>
2019-11-07 23:40:30 +0800

    ajax

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
     <div class="banner" @click="handleBannerClick">
-        <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1701/6c/6c5e1f8fa365adada3.img.png_600x330_51efe2f1.png" /> 
+        <img class="banner-img" :src="bannerImg" /> 
         <div class="banner-info">
-            <div class="banner-title">宁波方特东方神画</div>
-            <div class="banner-number banner-icon"><span class="iconfont" >&#xe6e1;</span>39</div>
+            <div class="banner-title">{{this.sightName}}</div>
+            <div class="banner-number banner-icon"><span class="iconfont" >&#xe6e1;</span>{{this.bannerImgs.length}}</div>
         </div>
     </div>
-    <common-gallary @close="handleGallaryClose" :imgs="imgs" v-show="showGallary"></common-gallary>
+    <common-gallary @close="handleGallaryClose" :imgs="bannerImgs" v-show="showGallary"></common-gallary>
 </div>
 
 </template>
@@ -18,10 +18,16 @@ export default {
     components:{
         CommonGallary
     },
+    props:{
+        sightName:String,
+        bannerImg:String,
+        bannerImgs:Array
+
+    },
     data () {
         return {
             showGallary:false,
-            imgs:['http://img1.qunarzz.com/sight/p0/1701/6c/6c5e1f8fa365adada3.img.png_r_800x800_bcbc8092.png','http://img1.qunarzz.com/sight/p0/1808/d1/d137655f0353c42ea3.img.png_r_800x800_1f755d3f.png']
+            //imgs:['http://img1.qunarzz.com/sight/p0/1701/6c/6c5e1f8fa365adada3.img.png_r_800x800_bcbc8092.png','http://img1.qunarzz.com/sight/p0/1808/d1/d137655f0353c42ea3.img.png_r_800x800_1f755d3f.png']
         }
     },
     methods:{
